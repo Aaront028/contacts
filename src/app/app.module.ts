57234ed0222845ff4f5259768c0ccebc64d0cf0b
undefined <info@aarontan.co.nz>
2023-10-02 17:01:48 +1300

    edit contact functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
 import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contacts/contact-details/contact-details.component.ts
+++ src/app/contacts/contact-details/contact-details.component.ts
@@ -1,13 +1,34 @@
 import { Component, Input } from '@angular/core';
 import { Contact } from '../contact.model';
+
 @Component({
   selector: 'app-contact-details',
   templateUrl: './contact-details.component.html',
   styleUrls: ['./contact-details.component.scss']
 })
 export class ContactDetailsComponent {
+  editing: boolean = false;
+  editedName: string = '';
+  editedEmail: string = '';
+  editedPhone: string = '';
+
   ngOnChanges() {
     console.log('Input Contact:', this.contact);
   }
   @Input() contact: Contact = new Contact('', '', '');
+
+  startEditing() {
+    this.editing = true;
+    this.editedName = this.contact.name;
+    this.editedEmail = this.contact.email;
+    this.editedPhone = this.contact.phone;
+  }
+
+  stopEditing(){
+    this.editing = false;
+    this.contact.name = this.editedName;
+    this.contact.email = this.editedEmail;
+    this.contact.phone = this.editedPhone;
+
+  }
 }
