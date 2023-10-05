cf7b2bbdd1f32898473a7973c27d85625587106b
undefined <info@aarontan.co.nz>
2023-10-05 13:31:47 +1300

    Remove environment.ts from tracking

--- src/environment/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://concise-crane-63.hasura.app/v1/graphql',
-  hasuraAdminSecret: '6ZLXnrFDilTCH2qRmOpr2tjuv4uMl8u4ukaP12Piv2WctnhQ6TqV7Vv6wZ3vc5PV',
-};
